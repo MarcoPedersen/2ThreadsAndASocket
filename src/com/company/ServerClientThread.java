381887b5e018802f7ab8355d72b8a81a7228bff2
MarcoPedersen <44088285+MarcoPedersen@users.noreply.github.com>
2019-10-17 10:41:33 +0200

    Initial commit

---
+++ src/com/company/ServerClientThread.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+class ServerClientThread extends Thread {
+    Socket serverClient;
+    int clientNo;
+    int squre;
+    ServerClientThread(Socket inSocket, int counter){
+        serverClient = inSocket;
+        clientNo=counter;
+    }
+    public void run(){
+        try{
+            DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+            DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
+            String clientMessage="", serverMessage="";
+            while(!clientMessage.equals("farvel")){
+                clientMessage=inStream.readUTF();
+                System.out.println("Fra Client-" +clientNo+ ": beskeden er :"+clientMessage);
+                serverMessage="Fra Server til Client-" + clientNo + " beskeden er " + clientMessage + " og kommer fra " +clientNo;
+                outStream.writeUTF(serverMessage);
+                outStream.flush();
+            }
+            inStream.close();
+            outStream.close();
+            serverClient.close();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }finally{
+            System.out.println("Client -" + clientNo + " smutter!! ");
+        }
+    }
+}
---
+++ src/com/company/TCPClient2.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPClient2 {
+    public static void main(String[] args) throws Exception {
+        try{
+            Socket socket=new Socket("127.0.0.1",8888);
+            DataInputStream inStream=new DataInputStream(socket.getInputStream());
+            DataOutputStream outStream=new DataOutputStream(socket.getOutputStream());
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            String clientMessage="",serverMessage="";
+            while(!clientMessage.equals("farvel")){
+                clientMessage=br.readLine();
+                outStream.writeUTF(clientMessage);
+                outStream.flush();
+                serverMessage=inStream.readUTF();
+                System.out.println("Fra Server: "+serverMessage);
+            }
+            outStream.close();
+            outStream.close();
+            socket.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
